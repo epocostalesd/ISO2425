8f598b578d6b51e72ed8fcd70ab727bf7cb78ba5
epocostalesd <eaposcostalesd01@iescastelar.com>
2024-11-18 09:50:58 +0100

    Add files via upload

---
+++ ProcesosPorLotes/mkprofile.bat
@@ -0,0 +1,15 @@
+@echo off
+rem script mkprofile.bat
+rem Emilio Pocostales de la Rosa
+rem 13/11/024
+mkdir %userprofile%\asir %userprofile%\dam %userprofile%\smr %userprofile%\daw %userprofile%\ceti
+
+mkdir %userprofile%\asir\docs %userprofile%\asir\pract %userprofile%\asir\exam
+
+mkdir %userprofile%\dam\docs %userprofile%\dam\pract %userprofile%\dam\exam
+
+mkdir %userprofile%\smr\docs %userprofile%\smr\pract %userprofile%\smr\exam
+
+mkdir %userprofile%\daw\docs %userprofile%\daw\pract %userprofile%\daw\exam
+
+mkdir %userprofile%\ceti\docs %userprofile%\ceti\pract %userprofile%\ceti\exam
